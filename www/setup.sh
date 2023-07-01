c27ec2efbd83cd54c093e88a054addac5cebe5b2
chrispab <cbattisson@gmail.com>
2023-07-01 21:49:36 +0100

    rename

--- www/setup.sh
+++ www/setup.sh
@@ -4,22 +4,22 @@
 # service cron start                  # start some services
 
 
-echo "setting up website"
+echo "setting up website pimcore file permissions"
 # sed -i 's/"host" => "localhost"/"host" => "mariadb"/' /var/www/vhosts/website/website/var/config/system.php
 cd /var/www/vhosts/website
 chown -R www-data:www-data pimcore website/var
 
-echo "setting up intranet"
+echo "setting up intranet pimcore file permissions"
 # sed -i 's/"host" => "localhost"/"host" => "mariadb"/' /var/www/vhosts/intranet/website/var/config/system.php
 cd /var/www/vhosts/intranet
 chown -R www-data:www-data pimcore website/var
 
-echo "setting up gotri"
+echo "setting up gotri pimcore file permissions"
 # sed -i 's/"host" => "db_server_10_0_0_38"/"host" => "mariadb"/' /var/www/vhosts/gotri_new/website/var/config/system.php
 cd /var/www/vhosts/gotri_new
 chown -R www-data:www-data pimcore website/var
 
-echo "setting up sbr"
+echo "setting up sbr pimcore file permissions"
 # sed -i 's/"host" => "10.0.0.3"/"host" => "mariadb"/' /var/www/vhosts/sbr/website/var/config/system.php
 cd /var/www/vhosts/sbr
 chown -R www-data:www-data pimcore website/var
